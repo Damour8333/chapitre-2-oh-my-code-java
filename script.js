7ad5ecf49aa386e57f87dfc34248578d0cff2bb1
jean-françois DAMOUR <lecrie33@gmail.com>
2022-03-27 18:11:19 +0200

    stocke un nombre un texte concaténation

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 /* 1ere ligne js */
 /*ce ligne est un outil de débogage ouvrir la console avec l inspecteur */ 
-console.log('connecté!!!');
-document.getElementById('titre').style.color ="green";
+/*console.log('connecté!!!');
+/*document.getElementById('titre').style.color ="green";*/
+
+/*initialistion d une variable av le mot clé 'const ' = contenu  de la variable.*/
+
+
+/*const titre = document.getElementById('titre');/
+console.log(titre);/* console.log(titre); pour verifier la variable dans la console */  
+
+
+
+/*
+
+let score = 0;//je stocke un nombre
+console.log(score,'initialisation');
+
+score = score+1;//opération
+console.log (score,'aprés addition');//je vérifie*/
+
+
+
+//STOCKER UN TEXTE
+//je stocke une chaine de caractére (string)
+const cours ="javascript";
+console.log(cours, "cours");
+
+//concaténation
+const mot1 ="hello";
+const mot2 ="world";
+const phrase = mot1+""+mot2//concaténation
