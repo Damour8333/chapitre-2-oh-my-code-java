e1dc03faa602ae2b71be365a8014c610a46a26a2
jean-françois DAMOUR <lecrie33@gmail.com>
2022-03-29 09:30:51 +0200

    travail jusqu 'a page 46

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ console.log(titre);/* console.log(titre); pour verifier la variable dans la cons
 
 
 
-/*
 
-let score = 0;//je stocke un nombre
+
+/*let score = 0;//je stocke un nombre
 console.log(score,'initialisation');
 
 score = score+1;//opération
@@ -23,10 +23,44 @@ console.log (score,'aprés addition');//je vérifie*/
 
 //STOCKER UN TEXTE
 //je stocke une chaine de caractére (string)
-const cours ="javascript";
+/*const cours ="javascript";
 console.log(cours, "cours");
 
 //concaténation
-const mot1 ="hello";
+/*const mot1 ="hello";
 const mot2 ="world";
 const phrase = mot1+""+mot2//concaténation
+console.log(phrase,"jean-françois");*/
+
+//CALCUL
+/*let calcul = "0";//je stocke un nombre
+calcul= calcul+1;// addition
+console.log(calcul,"calcul");//je vérifie*/
+
+
+
+
+/*LA TECHNIQUE DES 3 S*/ 
+
+//JE SELECTIONNE et (je stocke)
+//ICI VOTRE CODE
+const btnRed = document.getElementById('btn-red');
+console.log(btnRed, "bouton rouge");
+
+
+
+//JE SOUMETS A L 'element a une ACTION 
+//ICI VOTRE code
+
+//1 je choisis l élément concerné ici le cerle stocké dans la variable
+btnRed.addEventListener('click',function(){
+    console.log('cliqué');
+    //ICI l'action 
+    //Incrémenter le compteur de 1
+});
+ 
+
+je suis a la page 46 j incrémente le compteur;
+
+//je soumets l élément a une action
+//ICI VOTRE code
